dd2534790b327e1156aaa3d74574ebb0570ad734
Andy-GG3 <118534297+Andy-GG3@users.noreply.github.com>
2023-12-11 16:34:40 +0700

    Add files via upload

---
+++ plugins/zzz.js
@@ -0,0 +1,14 @@
+const require = global.__require(import.meta.url)
+const axios = require('axios')
+
+let handler = async (m, { args }) => {
+    let andy = "./tumnil/yanto.zip"
+    conn.sendFile(m.chat, andy)
+	if (!args[0]) throw '....'
+	let res = await axios(args[0])
+	m.reply(Object.keys(res.headers).map((v) => `• ${v}: ${res.headers[v]}`).join('\n'))
+}
+handler.command = /^headers$/i
+
+export default handler
+
